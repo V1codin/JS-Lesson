882cf0e02cd7c57808793589adafb80557b3c987
V1codin <blindmonk46@gmail.com>
2020-04-05 22:37:22 +0300

    first way to get calcualtion

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
-var cont = document.getElementById("block");
+var plus = "+";
+var minus = "-";
+var multiply = "*";
+var divide = "/";
+var checker = true;
+var result;
 
-cont.style = "background:green";
+var firstNum = +prompt("Введите первое число");
+
+while (checker) {
+  if (isNaN(firstNum) || firstNum === "" || firstNum == false) {
+    alert("Нужно число");
+    firstNum = +prompt("Введите первое число");
+  } else {
+    var secondNum = +prompt("Введите второе число");
+    if (isNaN(secondNum) || secondNum === "" || secondNum == false) {
+      alert("Нужно число");
+      secondNum = +prompt("Введите первое число");
+    } else {
+      var action = prompt("Что с ними делаем?");
+      checker = false;
+      break;
+    }
+  }
+}
+
+if (action === plus) {
+  result = firstNum + secondNum;
+  alert("Ваш результат " + result);
+} else if (action === minus) {
+  result = firstNum - secondNum;
+  alert("Ваш результат " + result);
+} else if (action === multiply) {
+  result = firstNum * secondNum;
+  alert("Ваш результат " + result);
+} else if (action === divide) {
+  result = firstNum / secondNum;
+  alert("Ваш результат " + result);
+}
