3a51ab2c8c76857f4fcb0d97692024ab927e1f48
V1codin <blindmonk46@gmail.com>
2020-04-15 22:52:45 +0300

    only start

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 var list = document.getElementById("list");
-var userText = prompt("Enter your list of words and wrapper pattern");
+// var userText = prompt("Enter your list of words and wrapper pattern");
+var userText = "Text. Valera. Check";
 
-function wrapping(arr, tag) {
+function wrapping(str, tag) {
   var wrappedStr = "";
-  if (arr.length > 0) {
-    for (item of arr) {
-      item = `<${tag}>${item}</${tag}>`;
+
+  for (item of str) {
+    if (item < "a" && item !== " " && item !== ".") {
+      wrappedStr += `<${tag}>`;
+    }
+    if (item !== ".") {
       wrappedStr += item;
+    } else {
+      wrappedStr += `.</${tag}>`;
     }
   }
+  console.log(wrappedStr);
   return wrappedStr;
 }
+
+wrapping(userText, "p");
+
+// console.log("A" > "a");
+
+// console.log("A".codePointAt(0));
+// console.log("a".codePointAt(0));
