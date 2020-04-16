5e91cc799a729f79dd927d23f2d1bbcac81bb4c5
V1codin <blindmonk46@gmail.com>
2020-04-16 21:22:06 +0300

    starting

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
 var out = document.getElementById("out");
-var userText = prompt("Enter your list of words and wrapper pattern");
-
-function checkingChars(str) {
-  if (str) {
-    var checkedStr = "";
-
-    for (item of str) {
-      if (
-        (item >= "A" && item <= "z") ||
-        (item >= "а" && item <= "і") ||
-        item === "." ||
-        item === " " ||
-        item === "," ||
-        isFinite(item)
-      ) {
-        checkedStr += item;
-      }
-    }
-
-    return checkedStr;
-  } else {
-    alert("You missed to input data");
-  }
-}
-
-out.innerHTML = checkingChars(userText);
+// var userText = prompt("Enter your list of words and wrapper pattern");
+// var userWords = prompt("Enter your list of words and wrapper pattern");
