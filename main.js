ae80c0f86e04ba6ada1fe1aa26bc148d9dfb6738
V1codin <blindmonk46@gmail.com>
2020-04-17 20:21:24 +0300

    little adjustment

--- main.js
+++ main.js
@@ -3,35 +3,35 @@ var userText = prompt("Enter text");
 var userWords = prompt("Enter words for check");
 
 function checkingWOrds(strText, strWords) {
-  var resultObj = {};
-
-  var result = "";
-
-  var tempArrText = [];
-
-  var tempArrWords = [];
-
-  function creatingArrWithReplace(str) {
-    var tempStrText = "";
-    var arr = [];
-    for (item of str) {
-      if (item === ".") {
-        item = item.replace(".", " ");
-      } else if (item === ",") {
-        item = item.replace(",", " ");
-      }
-      tempStrText += item;
-      arr = tempStrText.split(" ");
-    }
-    return arr;
-  }
-  function addingDataInObj(obj, arr) {
-    for (item of arr) {
-      obj[item] = 0;
-    }
-  }
-
   if (strText) {
+    var resultObj = {};
+
+    var result = "";
+
+    var tempArrText = [];
+
+    var tempArrWords = [];
+
+    function creatingArrWithReplace(str) {
+      var tempStrText = "";
+      var arr = [];
+      for (item of str) {
+        if (item === ".") {
+          item = item.replace(".", " ");
+        } else if (item === ",") {
+          item = item.replace(",", " ");
+        }
+        tempStrText += item;
+        arr = tempStrText.split(" ");
+      }
+      return arr;
+    }
+    function addingDataInObj(obj, arr) {
+      for (item of arr) {
+        obj[item] = 0;
+      }
+    }
+
     tempArrText = creatingArrWithReplace(strText);
   } else {
     alert("You missed to input data");
@@ -42,6 +42,7 @@ function checkingWOrds(strText, strWords) {
     alert("You missed to input data");
   }
   addingDataInObj(resultObj, tempArrWords);
+
   for (item in resultObj) {
     for (i of tempArrText) {
       if (item === i) {
