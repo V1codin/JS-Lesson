88f6ac34351ee1d48f9ce985bdcb7235cda17fa3
V1codin <blindmonk46@gmail.com>
2020-05-05 20:33:54 +0300

    ''

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 var out = document.getElementById("out");
+var newBlock = document.getElementById("newBlock");
+
 var btn = document.getElementById("btn");
+var clear = document.getElementById("btnClear");
+
 var inp = document.getElementById("inp");
+
+var blocks = {
+  nums: 0,
+};
+
+clear.onclick = function clear() {
+  out.style = "display: none";
+  blocks.nums = 0;
+  out.innerHTML = null;
+};
+
+btn.onclick = function createBlock() {
+  out.style = "display: block";
+  blocks.nums++;
+
+  var newDiv = document.createElement("div");
+  newDiv.id = "newBlock";
+  newDiv.innerText = `${blocks.nums} ${inp.value}`;
+
+  out.insertBefore(newDiv, newBlock);
+};
