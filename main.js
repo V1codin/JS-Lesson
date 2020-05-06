2d14643ea5d92fb0a199ee21219b8a257fb5bd8c
V1codin <blindmonk46@gmail.com>
2020-05-06 19:48:31 +0300

    'n'

--- main.js
+++ main.js
@@ -1,28 +1,69 @@
-var out = document.getElementById("out");
 var newBlock = document.getElementById("newBlock");
 
-var btn = document.getElementById("btn");
-var clear = document.getElementById("btnClear");
+var blockArr = [];
 
-var inp = document.getElementById("inp");
+function renderControls() {
+  var box = document.createElement("div");
+  box.className = "box";
+  box = document.body.appendChild(box);
 
-var blocks = {
-  nums: 0,
+  var cont = document.createElement("div");
+  cont.className = "container";
+  cont = document.body.appendChild(cont);
+
+  var outBlock = document.createElement("div");
+  outBlock.id = "out";
+  outBlock = cont.appendChild(outBlock);
+
+  var input = document.createElement("input");
+  input.type = "text";
+  input.id = "inp";
+  input = box.appendChild(input);
+
+  var pushBtn = document.createElement("button");
+  pushBtn.id = "btn";
+  pushBtn.innerText = "Push";
+  pushBtn.className = "push";
+  pushBtn = box.appendChild(pushBtn);
+
+  var clearBtn = document.createElement("button");
+  clearBtn.id = "btnClear";
+  clearBtn.innerText = "Clear";
+  clearBtn.className = "clear";
+  clearBtn = box.appendChild(clearBtn);
+  return { pushBtn, clearBtn, input, outBlock };
+}
+
+var controls = renderControls();
+
+controls.clearBtn.onclick = function clear() {
+  blockArr.length = 0;
+  controls.input.value = null;
+  controls.outBlock.style = "display: none";
+  controls.outBlock.innerHTML = null;
 };
 
-clear.onclick = function clear() {
-  out.style = "display: none";
-  blocks.nums = 0;
-  out.innerHTML = null;
-};
+controls.pushBtn.onclick = function createBlock() {
+  controls.outBlock.style = "display: block";
 
-btn.onclick = function createBlock() {
-  out.style = "display: block";
-  blocks.nums++;
+  var inpValue = controls.input.value;
 
   var newDiv = document.createElement("div");
   newDiv.id = "newBlock";
-  newDiv.innerText = `${blocks.nums} ${inp.value}`;
 
-  out.insertBefore(newDiv, newBlock);
+  console.log("newDiv: ", newDiv);
+
+  newDiv.name = inpValue;
+
+  if (blockArr.every((item) => item.name !== inpValue)) {
+    blockArr.push(newDiv);
+    newDiv.innerText = `${inpValue}. Index of this block : ${blockArr.indexOf(
+      newDiv
+    )}`;
+    controls.outBlock.insertBefore(newDiv, newBlock);
+    controls.input.value = null;
+  } else {
+    alert("The block exists");
+    controls.input.value = null;
+  }
 };
