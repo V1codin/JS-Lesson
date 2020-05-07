43526667d09ce79ee2c8244fda9bde0745157bbd
V1codin <blindmonk46@gmail.com>
2020-05-08 00:46:46 +0300

    task 8

--- main.js
+++ main.js
@@ -1,66 +1,48 @@
-var newBlock = document.getElementById("newBlock");
-
-var blockArr = [];
-
-function renderControls() {
-  var box = document.createElement("div");
-  box.className = "box";
-  box = document.body.appendChild(box);
-
-  var cont = document.createElement("div");
-  cont.className = "container";
-  cont = document.body.appendChild(cont);
-
-  var outBlock = document.createElement("div");
-  outBlock.id = "out";
-  outBlock = cont.appendChild(outBlock);
-
-  var input = document.createElement("input");
-  input.type = "text";
-  input.id = "inp";
-  input = box.appendChild(input);
-
-  var pushBtn = document.createElement("button");
-  pushBtn.id = "btn";
-  pushBtn.innerText = "Push";
-  pushBtn.className = "push";
-  pushBtn = box.appendChild(pushBtn);
-
-  var clearBtn = document.createElement("button");
-  clearBtn.id = "btnClear";
-  clearBtn.innerText = "Clear";
-  clearBtn.className = "clear";
-  clearBtn = box.appendChild(clearBtn);
-  return { pushBtn, clearBtn, input, outBlock };
-}
-var controls = renderControls();
-
-controls.clearBtn.onclick = function clear() {
-  blockArr.length = 0;
-  controls.input.value = null;
-  controls.outBlock.style = "display: none";
-  controls.outBlock.innerHTML = null;
+var controls = {
+  userInput: document.querySelector("#usertext"),
+  blockedInp: document.querySelector("#blockedInp"),
+  readOnly: document.querySelector("#read"),
+  numbers: document.querySelector("#numbers"),
+  pass: document.querySelector("#pass"),
+  checkers: document.querySelectorAll(".check"),
 };
 
-controls.pushBtn.onclick = function createBlock() {
-  controls.outBlock.style = "display: block";
-
-  var inpValue = controls.input.value;
-
-  var newDiv = document.createElement("div");
-  newDiv.id = "newBlock";
-
-  newDiv.name = inpValue;
-
-  if (blockArr.every((item) => item.name !== inpValue)) {
-    blockArr.push(newDiv);
-    newDiv.innerText = `${inpValue}. Index of this block : ${blockArr.indexOf(
-      newDiv
-    )}`;
-    controls.outBlock.insertBefore(newDiv, newBlock);
-    controls.input.value = null;
+controls.blockedInp.onchange = () => {
+  if (controls.blockedInp.checked) {
+    controls.checkers.forEach((item) => (item.checked = false));
+    controls.blockedInp.checked = true;
+    controls.userInput.disabled = true;
   } else {
-    alert("The block exists");
-    controls.input.value = null;
+    controls.userInput.disabled = false;
+  }
+};
+
+controls.readOnly.onchange = () => {
+  if (controls.readOnly.checked) {
+    controls.checkers.forEach((item) => (item.checked = false));
+    controls.readOnly.checked = true;
+    controls.userInput.readOnly = true;
+  } else {
+    controls.userInput.readOnly = false;
+  }
+};
+
+controls.numbers.onchange = () => {
+  if (controls.numbers.checked) {
+    controls.checkers.forEach((item) => (item.checked = false));
+    controls.numbers.checked = true;
+    controls.userInput.type = "number";
+  } else {
+    controls.userInput.type = "text";
+  }
+};
+
+controls.pass.onchange = () => {
+  if (controls.pass.checked) {
+    controls.checkers.forEach((item) => (item.checked = false));
+    controls.pass.checked = true;
+    controls.userInput.type = "password";
+  } else {
+    controls.userInput.type = "text";
   }
 };
