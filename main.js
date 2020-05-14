6e08f7924e46f06a48bf896b6173f5c0fcf4e1a8
V1codin <blindmonk46@gmail.com>
2020-05-14 19:17:56 +0300

    testing

--- main.js
+++ main.js
@@ -1,55 +1,145 @@
 var controls = {
+  box: document.querySelector(".box"),
   select: document.querySelector(".select"),
   input: document.querySelector(".inp"),
-};
+  invalidPoint: [],
+  newDiv: document.createElement("p"),
 
-controls.select.onchange = () => {
-  console.dir(controls.select[controls.select.selectedIndex].innerText);
-  console.log(controls.select.selectedIndex);
-  switch (controls.select.value) {
-    case "every":
-      var s = "s";
-  }
-};
+  // =============================
+  /*
+  comparing: function (str, arr) {
+    var check = 0;
 
-controls.input.oninput = (event) => {
-  var invalidPoint = "";
-
-  console.log(event.data);
-
-  if (
-    (event.data !== null && event.data.charCodeAt() < 65) ||
-    (event.data.charCodeAt() > 122 && event.data.charCodeAt() < 1040) ||
-    event.data.charCodeAt() > 1111
-  ) {
-    invalidPoint += event.data;
-    alert(
-      `You can input ${
-        controls.select[controls.select.selectedIndex].innerText
-      }`
-    );
-  }
-
-  if (controls.input.value.includes(invalidPoint) && invalidPoint !== "") {
-    controls.input.classList.add("error");
-  } else {
-    controls.input.classList.remove("error");
-  }
-  console.log("invalidPoint: ", invalidPoint);
-  console.log("value", controls.input.value);
-};
-
-function onlyLetters(str) {
-  var nStr = "";
-  for (char of str) {
-    if (
-      (char.charCodeAt() >= 65 && char.charCodeAt() <= 122) ||
-      (char.charCodeAt() >= 1040 && char.charCodeAt() <= 1100)
-    ) {
-      nStr += char;
+    for (char of str) {
+      for (item of arr) {
+        if (char === item) {
+          check++;
+        }
+      }
+    }
+    if (check > 0) {
+      return true;
     } else {
-      continue;
+      return false;
+    }
+  },
+  */
+};
+
+function renderWarning(parent, child, warningText) {
+  child.innerText = warningText;
+  child = parent.insertBefore(child, parent.firstElementChild);
+}
+
+function comparing(str, arr) {
+  var check = 0;
+
+  for (char of str) {
+    for (item of arr) {
+      if (char === item) {
+        check++;
+      }
     }
   }
-  return nStr;
+  if (check > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+function onlyNums(event) {
+  if (event.data !== null) {
+    if (event.data.charCodeAt() < 48 || event.data.charCodeAt() > 57) {
+      renderWarning(
+        controls.box,
+        controls.newDiv,
+        controls.select[controls.select.selectedIndex].innerText
+      );
+      controls.invalidPoint.push(event.data);
+      controls.input.classList.add("error");
+    }
+    if (controls.invalidPoint.length) {
+      var checker = comparing(controls.input.value, controls.invalidPoint);
+      if (!checker) {
+        controls.input.classList.remove("error");
+        controls.invalidPoint.length = 0;
+        controls.box.removeChild(controls.newDiv);
+      }
+    }
+  }
+}
+
+function onlyLetters(event) {
+  if (event.data !== null) {
+    if (
+      event.data.charCodeAt() < 65 ||
+      (event.data.charCodeAt() > 122 && event.data.charCodeAt() < 1040) ||
+      event.data.charCodeAt() > 1111
+    ) {
+      renderWarning(
+        controls.box,
+        controls.newDiv,
+        controls.select[controls.select.selectedIndex].innerText
+      );
+      controls.invalidPoint.push(event.data);
+      controls.input.classList.add("error");
+    }
+    if (controls.invalidPoint.length) {
+      var checker = comparing(controls.input.value, controls.invalidPoint);
+      if (!checker) {
+        controls.input.classList.remove("error");
+        controls.invalidPoint.length = 0;
+        controls.box.removeChild(controls.newDiv);
+      }
+    }
+  }
+}
+
+function every(event) {
+  if (event.data !== null) {
+    if (
+      (event.data.charCodeAt() < 48 &&
+        event.data.charCodeAt() !== 44 &&
+        event.data.charCodeAt() !== 46) ||
+      (event.data.charCodeAt() > 57 && event.data.charCodeAt() < 65) ||
+      (event.data.charCodeAt() > 122 && event.data.charCodeAt() < 1040) ||
+      event.data.charCodeAt() > 1111
+    ) {
+      renderWarning(
+        controls.box,
+        controls.newDiv,
+        controls.select[controls.select.selectedIndex].innerText
+      );
+      controls.invalidPoint.push(event.data);
+      controls.input.classList.add("error");
+    }
+    if (controls.invalidPoint.length) {
+      var checker = comparing(controls.input.value, controls.invalidPoint);
+      if (!checker) {
+        controls.input.classList.remove("error");
+        controls.invalidPoint.length = 0;
+        controls.box.removeChild(controls.newDiv);
+      }
+    }
+  }
+}
+
+controls.select.onchange = () => {
+  console.log(controls.select.selectedIndex);
+
+  switch (controls.select.selectedIndex) {
+    case 1:
+      controls.input.value = null;
+      controls.input.oninput = every;
+      break;
+    case 2:
+      controls.input.value = null;
+      controls.input.oninput = onlyLetters;
+      break;
+    case 3:
+      controls.input.value = null;
+      controls.input.oninput = onlyNums;
+      break;
+  }
+};
