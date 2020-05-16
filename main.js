758c951bb16331df241f8395dbc5c21da0aee297
V1codin <blindmonk46@gmail.com>
2020-05-17 00:32:42 +0300

    trying to adjust email validation

--- main.js
+++ main.js
@@ -2,13 +2,75 @@ var ctr = {
   form: document.querySelector(".form"),
   inputTyping: document.querySelectorAll(".typing"),
   wrappers: document.querySelectorAll(".wrapper"),
+  submitBtn: document.querySelector(".submit"),
+  warnings: {
+    name: `Имя должно содержать только Буквы нижнего и верхнего регистра. Обязательное поле`,
+    age: `Возраст должен быть от 7 до 99 лет`,
+    mail: `Почта должна содержать только латинские символы, собачку, точку, нижнее подчёркивание. Обязательное поле`,
+  },
 };
 
-var warnings = {
-  name: `Имя должно содержать только Буквы нижнего и верхнего регистра. Обязательное поле`,
-  age: `Возраст должен быть от 7 до 99 лет`,
-  mail: `Почта должна содержать только латинские символы, собачку, точку, нижнее подчёркивание. Обязательное поле`,
+ctr.inputTyping.forEach((item) => {
+  item.onfocus = focusFN;
+  item.onblur = blurFN;
+});
+
+ctr.submitBtn.onclick = (event) => {
+  event.preventDefault();
+  var check = gettingValues(ctr.form);
+  var test = validation(check, rules);
+  // console.log(check);
+  console.log(test);
 };
+
+function error() {
+  alert("Введите корректные данные");
+}
+
+function validation(valueObj, rulesObj) {
+  var checkArr = [];
+
+  for (item in valueObj) {
+    for (prop in rulesObj) {
+      if (item === prop) {
+        console.log(item);
+        checkArr.push(rulesObj[prop].test(valueObj[item]));
+      }
+    }
+  }
+  if (valueObj.age < 7) {
+    return error();
+  }
+
+  for (check of checkArr) {
+    if (!check) {
+      return error();
+    }
+  }
+  return checkArr;
+}
+
+var rules = {
+  age: /^\d{1,2}$/,
+  name: /^[A-Z][a-z]+$/,
+
+  mail: /^[a-z]+(\.{1}|\_|[a-z]+)[a-z]+(\.{1}|\_|[a-z]+)[a-z]+$/,
+  // mail: /^[a-z]{1,}(\.{1}|\_|[a-z]{1,})[a-z]{1,}(\.{1}|\_|[a-z]{1,})[a-z]+$/,
+  checker: [],
+};
+
+function gettingValues(form) {
+  var valueObj = {};
+
+  for (i = 0; i < form.length; i++) {
+    if (form[i].tagName !== "BUTTON" && form[i].value !== "subscribe") {
+      valueObj[form[i].name] = form[i].value;
+    } else if (form[i].value === "subscribe") {
+      valueObj["subscribe-checkbox"] = form[i].checked;
+    }
+  }
+  return valueObj;
+}
 
 function renderWarning(parent, child, warningText) {
   child = document.createElement("span");
@@ -18,20 +80,20 @@ function renderWarning(parent, child, warningText) {
   child.style = "display: inline-block";
 }
 
-ctr.inputTyping.forEach((item) => (item.onfocus = focusFN));
-ctr.inputTyping.forEach((item) => (item.onblur = blurFN));
-
 function focusFN() {
   var parent = this.parentElement;
-  renderWarning(parent, "warning", warnings[this.name]);
+  renderWarning(parent, "warning", ctr.warnings[this.name]);
 }
 
-function blurFN() {
+function blurFN(obj) {
+  obj[this.value] = this.value;
   var parent = this.parentElement;
   parent.lastElementChild.remove();
 }
 
-function onlyLetters(event) {
+/*
+
+function onlyLetters() {
   if (this.value) {
     for (char of this.value) {
       if (
@@ -39,20 +101,34 @@ function onlyLetters(event) {
         (char.charCodeAt() > 122 && char.charCodeAt() < 1040) ||
         char.charCodeAt() > 1111
       ) {
-        alert("Введите корректные данные");
+        rules.checker.push(false);
       } else {
-        return true;
+        rules.checker.push(true);
       }
     }
   } else {
-    alert("Введите корректные данные");
+    rules.checker.push(false);
   }
 }
 
-function onlyNums(event) {
-  if (this.value >= 7) {
-    return true;
-  } else {
-    alert("Введите корректные данные");
+function onlyNums(validation = rules.age) {
+  if (this.value) {
+    var validator = this.value.match(validation);
+    console.log(validator);
+
+    if (this.value >= 7 && this.value == validator) {
+      rules.checker.push(true);
+    } else {
+      rules.checker.push(false);
+    }
   }
 }
+*/
+
+/*
+var validator = /^\w/;
+
+var test = "s";
+
+var test2 = test.match(validator);
+*/
