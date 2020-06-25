a5f022ad2eb2722a5751959ed93ab12f09ebe2bd
V1codin <blindmonk46@gmail.com>
2020-06-25 21:30:19 +0300

    adding post request

--- main.js
+++ main.js
@@ -2,11 +2,51 @@ class DeliveryData {
   constructor(inputId, buttonId) {
     this.dataInput = document.querySelector(`#${inputId}`);
     this.submitData = document.querySelector(`#${buttonId}`);
-    this.submitData.onclick = () => console.log("works");
+    this.submitData.onclick = () => {
+      console.log("url", this.myUrl);
+      console.log("api", this.myApi);
+      console.log("inp value", this.dataInput.value);
+
+      fetch(this.myUrl, {
+        method: "POST",
+        dataType: "json",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify({
+          apiKey: this.myApi,
+          modelName: "TrackingDocument",
+          calledMethod: "getStatusDocuments",
+          methodProperties: {
+            Documents: [
+              {
+                DocumentNumber: this.dataInput.value,
+                Phone: "",
+              },
+              {
+                DocumentNumber: this.dataInput.value,
+                Phone: "",
+              },
+            ],
+          },
+        }),
+      })
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => console.log(data));
+    };
+  }
+
+  displayTracking() {
+    console.log("track");
   }
 
   get myApi() {
-    return "e0922b249ed537a7bd58d7b9ad06f827";
+    return "04ddf8bdb4d9138f496898f46480d4a2";
+  }
+  get myUrl() {
+    return "https://api.novaposhta.ua/v2.0/json/";
   }
 }
 
