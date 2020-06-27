fa6d7b699f84f38e20b7624316b9717dd6f1e5a8
V1codin <blindmonk46@gmail.com>
2020-06-27 09:03:42 +0300

    'n'

--- main.js
+++ main.js
@@ -1,40 +1,51 @@
+// 59000527233015
+
 class DeliveryData {
-  constructor(inputId, buttonId) {
+  constructor(inputId, buttonId, outDataClass, historyDataClass) {
     this.dataInput = document.querySelector(`#${inputId}`);
     this.submitData = document.querySelector(`#${buttonId}`);
+
+    this.outDataContainer = document.querySelector(`.${outDataClass}`);
+    this.historyDataContainer = document.querySelector(`.${historyDataClass}`);
+
     this.submitData.onclick = () => {
       console.log("url", this.myUrl);
       console.log("api", this.myApi);
       console.log("inp value", this.dataInput.value);
 
-      fetch(this.myUrl, {
-        method: "POST",
-        dataType: "json",
-        headers: {
-          "Content-Type": "application/json;charset=utf-8",
-        },
-        body: JSON.stringify({
-          apiKey: this.myApi,
-          modelName: "TrackingDocument",
-          calledMethod: "getStatusDocuments",
-          methodProperties: {
-            Documents: [
-              {
-                DocumentNumber: this.dataInput.value,
-                Phone: "",
-              },
-              {
-                DocumentNumber: this.dataInput.value,
-                Phone: "",
-              },
-            ],
+      if (this.dataInput.value) {
+        fetch(this.myUrl, {
+          method: "POST",
+          dataType: "json",
+          headers: {
+            "Content-Type": "application/json;charset=utf-8",
           },
-        }),
-      })
-        .then((res) => {
-          return res.json();
+          body: JSON.stringify({
+            apiKey: this.myApi,
+            modelName: "TrackingDocument",
+            calledMethod: "getStatusDocuments",
+            methodProperties: {
+              Documents: [
+                {
+                  DocumentNumber: this.dataInput.value,
+                  Phone: "",
+                },
+                {
+                  DocumentNumber: this.dataInput.value,
+                  Phone: "",
+                },
+              ],
+            },
+          }),
         })
-        .then((data) => console.log(data));
+          .then((res) => {
+            return res.json();
+          })
+          .then((r) => console.log(r.data[0]));
+
+        this.outDataContainer.style = "display: block";
+        this.historyDataContainer.style = "display: block";
+      }
     };
   }
 
@@ -52,5 +63,7 @@ class DeliveryData {
 
 const test = new DeliveryData(
   "input-container_user-input",
-  "wrapper_submit-tracking"
+  "wrapper_submit-tracking",
+  "container_out",
+  "container_history"
 );
