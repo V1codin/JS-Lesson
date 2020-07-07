ca635c024fe270e110b8cd15ecaae749fea1c156
V1codin <blindmonk46@gmail.com>
2020-07-07 22:30:26 +0300

    added dotenv package

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  // entry: "./src/index.js",
+  entry: "./dist/main.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+  },
+  watchOptions: {
+    aggregateTimeout: 200,
+    poll: 1000,
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
   },
 };
